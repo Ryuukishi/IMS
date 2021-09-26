eed7ce71ff126834683e6a58eeee7103fb54c084
Ryuukishi <richardcho11@hotmail.com>
2021-09-27 09:50:29 +1000

    Created a CRUD controller module for managing the updating of inventory

---
+++ crud.rb
@@ -0,0 +1,65 @@
+require_relative "./inventory.rb"
+require 'tty-prompt'
+require 'csv'
+
+module Crud
+
+    # attr_accessor :headers, :row, :rows, :id, :inventory_hash, :inventory_record
+
+    @headers = ["Name", "Price", "Quantity"]
+    @row = []
+    @rows = []
+    @id = Inventory.id
+    @inventory_record = {}
+    @id_record = {}
+    
+
+    def self.create
+        prompt = TTY::Prompt.new
+        name = prompt.ask('Item name:', required: true)
+        @row << name
+        price = prompt.ask('Price:', required: true, convert: :float)
+        @row << price
+        quantity = prompt.ask('Quantity', required: true)
+        @row << quantity
+        inventory = Inventory.new(name, price, quantity)
+        inventory.print
+        @rows << @row
+        @row = []
+        @inventory_record[@id] = [name, price, quantity]
+        @id_record[name] = @id
+    end
+
+    def self.save
+        output = [@headers] + @rows
+        CSV.open('Inventory.csv', 'w') do |csv|
+            output.each { |arr| csv << arr }
+        end
+    end
+
+    def self.update
+        prompt = TTY::Prompt.new
+        name = prompt.ask('Item to change:', required: true)
+        if ! @id_record.include? name
+            puts "Item not in inventory!"
+            sleep(2)
+            self.update()
+        end
+        choices = [
+            {name: 'Change Price', value: 1},
+            {name: 'Change Quantity', value: 2},
+            {name: 'Finish', value: 3}
+            ]
+        answer = prompt.select('Select', choices, cycle: true)
+        case answer
+            when 1
+                price = prompt.ask('Price:', required: true)
+                @inventory_record[@id_record[name]][1] = price
+                self.save #need to rewrite the logic so that it takes the values from inventory_record and id_record instead of headers and rows...
+            when 2
+                quantity = prompt.ask('Quantity:', required: true)
+                @inventory_record[@id_record[name]][2] = quantity
+            when 3
+        end 
+    end
+end
