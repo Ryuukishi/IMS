963eeb59ca579b8a83d0a436f1843a2db7254455
Ryuukishi <richardcho11@hotmail.com>
2021-10-02 15:19:07 +1000

    Error handling to rescue NoMethodError when the user tries to update an item that does not exit in the inventory.

--- controller/crud.rb
+++ controller/crud.rb
@@ -67,11 +67,11 @@ module Crud
     def self.update
         prompt = TTY::Prompt.new
         name = prompt.ask('Item to change:', required: true)
-        if ! @id_record.include? name
-            puts "Item not in inventory!"
-            sleep(1)
-            self.update()
-        end
+        # if ! @id_record.include? name
+        #     puts "Item not in inventory!"
+        #     sleep(1)
+        #     self.update()
+        # end
         choices = [
             {name: 'Change Price', value: 1},
             {name: 'Change Quantity', value: 2},
@@ -82,8 +82,13 @@ module Crud
             answer = prompt.select('Select', choices, cycle: true)
             case answer
                 when 1
+                    begin # Error handling to rescue NoMethodError when the user tries to update an item that does not exit in the inventory.
                     price = prompt.ask('Price:', required: true)
                     @inventory_record[@id_record[name]][1] = price
+                    rescue NoMethodError
+                        puts "Item doesn't exist"
+                        self.update
+                    end
                 when 2
                     quantity = prompt.ask('Quantity:', required: true)
                     @inventory_record[@id_record[name]][2] = quantity
